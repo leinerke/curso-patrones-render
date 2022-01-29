3ce9d2c1b4746439ba5e847a332e1c36c3e8c0da
Leinerke Suarez <leinerke2000@gmail.com>
2022-01-29 17:56:04 -0500

    Composición de componentes con React Context.

--- src/components/App/AppUI.js
+++ src/components/App/AppUI.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { TodoContext } from "../../TodoContext";
+import { TodoHeader } from "../TodoHeader";
 import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
@@ -19,11 +20,23 @@ function AppUI() {
     completeTodo,
     deleteTodo,
     openModal,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
   } = React.useContext(TodoContext);
   return (
     <>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoHeader>
+        <TodoCounter
+          totalTodos={totalTodos}
+          completedTodos={completedTodos}
+        />
+        <TodoSearch
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </TodoHeader>
       <TodoList>
         {error && <TodoError />}
         {loading && <TodoLoading />}
